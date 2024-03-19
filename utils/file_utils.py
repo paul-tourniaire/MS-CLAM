813684f1cda340e25afe77f6ab9bb5285a71d56f
George Batchkala <george.batchkala@gmail.com>
2024-03-19 14:52:10 +0000

    add h5 file utils

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,6 +1,8 @@
 import pickle
 import h5py
 
+import numpy as np
+
 def save_pkl(filename, save_object):
 	writer = open(filename,'wb')
 	pickle.dump(save_object, writer)
@@ -33,3 +35,80 @@ def save_hdf5(output_path, asset_dict, attr_dict= None, mode='a'):
             dset[-data_shape[0]:] = val
     file.close()
     return output_path
+
+
+def print_h5_file_info(h5_file_path):
+    with h5py.File(h5_file_path, "r") as f:
+
+        print("File attributes:", f.attrs.keys())
+        print("File keys:", f.keys())
+
+        for i, big_key in enumerate(f.keys()):
+            print(f"Key {i}: <{big_key}>")
+            dset = f[big_key]
+            print(f"Dataset <{big_key}> length:", len(dset))
+            print(f"Dataset <{big_key}> shape:", dset.shape)
+
+            print(f"Dataset <{big_key}> attribute keys:", dset.attrs.keys())
+            print()
+
+            for key, value in dset.attrs.items():
+                print("\t", key, value)
+
+            print()
+            for i in range(10):
+                if big_key == "imgs":
+                    print(dset[i].shape)
+                else:
+                    print(dset[i].shape, dset[i])
+            print()
+
+
+def compare_attributes(obj1, obj2, obj_name, ignore_attributes):
+    if not set(obj1.attrs.keys()) == set(obj2.attrs.keys()):
+        print(
+            f"Attribute keys of <{obj_name}> in file1 {obj1.attrs.keys()} are not the same as in file2 {obj2.attrs.keys()}")
+        return False
+    for key in obj1.attrs.keys():
+        if not np.array_equal(obj1.attrs[key], obj2.attrs[key]):
+            print(f"Attributes for key <{key}> in {obj_name} not equal:")
+            print("\t", obj1.attrs[key])
+            print("\t", obj2.attrs[key])
+            if key in ignore_attributes:
+                print(f"Attribute key <{key}> inconsistency is ignored.")
+                continue
+            else:
+                return False
+    return True
+
+
+def check_h5_files_are_equal(file1, file2, atol, ignore_attributes=()):
+    with h5py.File(file1, 'r') as f1, h5py.File(file2, 'r') as f2:
+
+        # Check file attribute keys
+        if not compare_attributes(f1, f2, "file", ignore_attributes):
+            return False
+
+        # Check dataset keys
+        if not set(f1.keys()) == set(f2.keys()):
+            print(
+            	f"Dataset keys of file1 {f1.keys()} are not the same as dataset keys of file2 {f2.keys()}")
+            return False
+
+        # Check datasets and their attributes
+        for key in f1.keys():
+            dset1 = f1[key]
+            dset2 = f2[key]
+
+            # Check dataset attributes
+            if not compare_attributes(dset1, dset2, f"dataset <{key}>", ignore_attributes):
+                return False
+
+            # Check dataset values
+            if not np.allclose(dset1[:], dset2[:], atol=atol):
+                print(f"Datasets for key {key} not equal")
+                print(dset1[:] - dset2[:])
+                return False
+
+    print("The files are the same.")
+    return True
